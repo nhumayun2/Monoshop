7a4f06f13438554bc53b28423cf474cc8a8f0888
nhumayun2 <nhumayun291@gmail.com>
2024-12-30 18:09:50 +0600

    1st commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroSectionComponent } from './initialComponents/hero-section/hero-section.component';
+import { FeaturedProductsComponent } from './initialComponents/featured-products/featured-products.component';
+import { TestimonialsComponent } from './initialComponents/testimonials/testimonials.component';
+import { FooterComponent } from './initialComponents/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeroSectionComponent,FeaturedProductsComponent,TestimonialsComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: AppComponent},
+];
